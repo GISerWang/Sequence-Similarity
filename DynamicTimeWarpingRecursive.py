512e94a90d7c794eb287511ca65e8b4eb01acdf7
wpx <260129327@qq.com>
2019-06-25 21:36:40 +0800

    DTW算法的递归实现

--- DynamicTimeWarpingRecursive.py
+++ DynamicTimeWarpingRecursive.py
@@ -1,27 +1,63 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 from scipy.spatial.distance import cdist
+import time
+# 提取路径对齐
+def extractPath(costMatrix,i,j):
+    # 初始化路径
+    path = []
+    # 从右下角循环，寻找对齐点
+    while i != 0 and j != 0:
+        # 首先加入右下角点
+        path.insert(0, (i, j))
+        # 循环的元素，三个坐标
+        idxArr = [(i-1,j),(i-1,j-1),(i,j-1)]
+        # 寻找到最小的那个值
+        minArg = np.argmin(np.array([
+            costMatrix[i - 1][j],
+            costMatrix[i - 1][j - 1],
+            costMatrix[i][j - 1]]))
+        # 对应的消耗矩阵的元素
+        minIndex = idxArr[minArg]
+        # 重新迭代
+        i = minIndex[0]
+        j = minIndex[1]
+    # 寻找靠边的点
+    if i != 0 and j == 0:
+        while i != 0:
+            path.insert(0, (i, 0))
+            i = i-1
+    # 寻找靠边的点
+    if i == 0 and j != 0:
+        while j != 0:
+            path.insert(0, (0, j))
+            j = j - 1
+    # 加入0，0
+    if i == 0 and j == 0:
+        path.insert(0, (0, 0))
+        return path
 # 使用递归的方式求解cstMatrix的i,j的数值
-# 即cstMatrix右下角的最后一个值为Frechet距离
-def _dtw(disMat,cstMatrix,i,j):
+# 即cstMatrix右下角的最后一个值为dtw距离
+# 返回值：dtw值+路径
+def _dtw(disMat,costMatrix,i,j):
     # 如果cstMatrix[i][j]不等于-1，直接返回，不需要计算了（借助动态规划的思想）
-    if cstMatrix[i][j] > -1:
-        return cstMatrix[i][j]
+    if costMatrix[i][j] > -1:
+        return costMatrix[i][j]
     # 当i,j都等于0的时候，计算消耗矩阵的值
     if i == 0 and j == 0:
-        cstMatrix[i][j] = disMat[0][0]
+        costMatrix[i][j] = disMat[0][0]
     # 计算第一列的值
     if i > 0 and j == 0:
-        cstMatrix[i][j] = _dtw(disMat,cstMatrix, i - 1, 0) + disMat[i][0]
+        costMatrix[i][j] = _dtw(disMat, costMatrix, i - 1, 0) + disMat[i][0]
     # 计算第一行的值
     if i == 0 and j > 0:
-        cstMatrix[i][j] = _dtw(disMat,cstMatrix, 0, j - 1) + disMat[0][j]
+        costMatrix[i][j] = _dtw(disMat, costMatrix, 0, j - 1) + disMat[0][j]
     # 计算其他值
     if i > 0 and j > 0:
-        cstMatrix[i][j] = min(_dtw(disMat, cstMatrix, i, j-1),
-                              _dtw(disMat, cstMatrix, i - 1, j-1),
-                              _dtw(disMat, cstMatrix, i - 1, j)) + disMat[i][j]
-    return cstMatrix[i][j]
+        costMatrix[i][j] = min(_dtw(disMat, costMatrix, i, j-1),
+                               _dtw(disMat, costMatrix, i - 1, j-1),
+                               _dtw(disMat, costMatrix, i - 1, j)) + disMat[i][j]
+    return costMatrix[i][j]
 def DynamicTimeWarping(ptSetA, ptSetB):
     # 获得点集ptSetA中点的个数n
     n = ptSetA.shape[0]
@@ -30,13 +66,18 @@ def DynamicTimeWarping(ptSetA, ptSetB):
     # 计算任意两个点的距离矩阵
     disMat = cdist(ptSetA, ptSetB, metric='euclidean')
     # 初始化消耗矩阵
-    cstMatrix = np.full((n,m),-1.0)
+    costMatrix = np.full((n,m),-1.0)
     # 递归求解DTW距离
-    return _dtw(disMat,cstMatrix,n-1,m-1)
-
+    dtwDis = _dtw(disMat,costMatrix,n-1,m-1)
+    # 提取路径
+    path = extractPath(costMatrix,n-1,m-1)
+    return dtwDis,path
 data = np.loadtxt("./data/traj.csv",delimiter=",")
 # 加载三条轨迹
 traj1, traj2, traj3 = data[:8], data[8:15], data[15:]
-print("轨迹1与轨迹2的DTW距离为：%s"%(DynamicTimeWarping(traj1,traj2)))
-print("轨迹2与轨迹3的DTW距离为：%s"%(DynamicTimeWarping(traj2,traj3)))
-print("轨迹1与轨迹3的DTW距离为：%s"%(DynamicTimeWarping(traj1,traj3)))
+starttime = time.clock()
+print("轨迹1与轨迹2的DTW距离为：%s"%(DynamicTimeWarping(traj1,traj2)[0]))
+print("轨迹2与轨迹3的DTW距离为：%s"%(DynamicTimeWarping(traj2,traj3)[0]))
+print("轨迹1与轨迹3的DTW距离为：%s"%(DynamicTimeWarping(traj1,traj3)[0]))
+endtime = time.clock()
+print(endtime - starttime)
