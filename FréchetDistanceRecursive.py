5177f3983b38730f8e02ab86c56bb6f420d7cbcb
wpx <260129327@qq.com>
2019-06-25 19:48:38 +0800

    Frechet距离算法的递归实现

--- FréchetDistanceRecursive.py
+++ FréchetDistanceRecursive.py
@@ -3,7 +3,7 @@ import numpy as np
 from scipy.spatial.distance import cdist
 # 使用递归的方式求解cstMatrix的i,j的数值
 # 即cstMatrix右下角的最后一个值为Frechet距离
-def _recursive(disMat,cstMatrix,i,j):
+def _frechet(disMat,cstMatrix,i,j):
     # 如果cstMatrix[i][j]不等于-1，直接返回，不需要计算了（借助动态规划的思想）
     if cstMatrix[i][j] > -1:
         return cstMatrix[i][j]
@@ -12,13 +12,13 @@ def _recursive(disMat,cstMatrix,i,j):
         cstMatrix[i][j] = disMat[0][0]
     # 计算第一列的值
     if i > 0 and j == 0:
-        cstMatrix[i][j] = max(_recursive(disMat,cstMatrix, i - 1, 0), disMat[i][0])
+        cstMatrix[i][j] = max(_frechet(disMat,cstMatrix, i - 1, 0), disMat[i][0])
     # 计算第一行的值
     if i == 0 and j > 0:
-        cstMatrix[i][j] = max(_recursive(disMat,cstMatrix, 0, j - 1), disMat[0][j])
+        cstMatrix[i][j] = max(_frechet(disMat,cstMatrix, 0, j - 1), disMat[0][j])
     # 计算其他值
     if i > 0 and j > 0:
-        cstMatrix[i][j] = max(min(_recursive(disMat,cstMatrix, i - 1, j), _recursive(disMat,cstMatrix, i - 1, j - 1), _recursive(disMat,cstMatrix, i, j - 1)),
+        cstMatrix[i][j] = max(min(_frechet(disMat,cstMatrix, i - 1, j), _frechet(disMat,cstMatrix, i - 1, j - 1), _frechet(disMat,cstMatrix, i, j - 1)),
                               disMat[i][j])
     return cstMatrix[i][j]
 def FrechetDistance(ptSetA, ptSetB):
@@ -31,7 +31,7 @@ def FrechetDistance(ptSetA, ptSetB):
     # 初始化消耗矩阵
     cstMatrix = np.full((n,m),-1.0)
     # 递归求解Frechet距离
-    return _recursive(disMat,cstMatrix,n-1,m-1)
+    return _frechet(disMat,cstMatrix,n-1,m-1)
 data = np.loadtxt("./data/traj.csv",delimiter=",")
 # 加载三条轨迹
 traj1, traj2, traj3 = data[:8], data[8:15], data[15:]
